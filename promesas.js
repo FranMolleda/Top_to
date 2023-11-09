4a219957b6f11df91b9d0ddd0763973cefc27879
= <=>
2023-11-09 10:13:27 +0100

    prepara ndo el ejercicio

---
+++ promesas.js
@@ -0,0 +1,6 @@
+/* **Ejercicio de promesas**:
+   - Escribe una función que realice una solicitud AJAX a una API y devuelva los datos en forma de promesa. */
+
+document.addEventListener("DOMContentLoaded", function () {
+
+})
